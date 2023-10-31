82b3132af8e3915c443f71c38d55f56295828f23
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-10-31 21:28:23 +0000

    can create svg

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,1 +1,9 @@
 import Particle from "../classes/particle.js";
+import createParticleSVG from "./createSVG.js";
+
+const viewPort = document.querySelector('#view')
+
+const svg = createParticleSVG(10);
+
+viewPort.appendChild(svg);
+
