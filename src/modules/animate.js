a23f61683dfc357838cec94971b9284404b60f4c
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-11-01 21:48:38 +0000

    bounds detection yay!

--- src/modules/animate.js
+++ src/modules/animate.js
@@ -1,17 +1,26 @@
+import { viewPort } from "../global.js";
 import { calcParticleDisplacement, calcParticleVelocity } from "./engine.js";
 
 function animate(particles) {
     const currentTime = window.performance.now();
-    particles.forEach(particle => {
+    particles.forEach((particle, index) => {
         const deltaTime = (currentTime - particle.getTime()) / 1000;
+        const position = particle.getPosition();
         
         const displacement = calcParticleDisplacement(particle, deltaTime);
-        particle.setPosition(particle.getPosition().map((element, index) => element + displacement[index]));
+        particle.setPosition(position.map((element, index) => element + displacement[index]));
 
         const velocity = calcParticleVelocity(particle, deltaTime);
         particle.setVelocity(velocity);
 
-        console.log(particle.getVelocity());
+        if (particle.x < 0 || particle.x > viewPort.clientWidth ||
+            particle.y < 0 || particle.y > viewPort.clientHeight) {
+            particles.splice(index, 1);
+            particle.getCircle().remove();
+
+            console.log(particles);
+        }
+
     });
 
     requestAnimationFrame(() => animate(particles));
